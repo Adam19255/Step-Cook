6605c84352ac07a0d1a3a8f830f5ab422f09d792
Adam Shay <145905613+Adam19255@users.noreply.github.com>
2025-05-04 16:40:17 +0300

    Made the main activity a splash screen, added a login and register activity

--- app/src/main/java/com/project/step_cook/Register.java
+++ app/src/main/java/com/project/step_cook/Register.java
@@ -1,7 +1,7 @@
 package com.project.step_cook;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,20 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Intent intent = new Intent(MainActivity.this, Login.class);
-        startActivity(intent);
+    }
+
+    public void register(View v){
+        // do something
     }
 }
