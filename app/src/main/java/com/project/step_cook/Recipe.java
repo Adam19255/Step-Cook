84ed5e8128d69a72a91a2fdd9e1533bc93745847
Maxwell <maxims970@gmail.com>
2025-05-06 15:26:18 +0300

    Updated Recipe class & added SVG icons to resources

--- app/src/main/java/com/project/step_cook/Recipe.java
+++ app/src/main/java/com/project/step_cook/Recipe.java
@@ -7,7 +7,7 @@ public class Recipe {
     private int cookTime; // Cook time in seconds
     private String imageUrl;
     public Recipe() {}
-    public Recipe(String id, String title, String description, int cookTimeSeconds, String imageUrl) {
+    public Recipe(String id, String title, int cookTimeSeconds, String imageUrl) {
         this.id = id;
         this.title = title;
         this.cookTime = cookTimeSeconds;
